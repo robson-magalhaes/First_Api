ab42db0f1b4e0bd02f6370d64f0773e14e7f7cef
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-09-16 15:37:07 -0300

    Atualizando banco de dados e operações CRUD

--- Controllers/API/CategoriaApiController.cs
+++ Controllers/API/CategoriaApiController.cs
@@ -17,12 +17,33 @@ namespace UniApi.Controllers.API
         }
 
         [HttpPost("/categoria")]
-        public Categoria PostCategoria(Categoria prod)
+        public Categoria Post(Categoria prod)
         {
             _context.Categorias.Add(prod);
             _context.SaveChanges();
             return prod;
         }
 
+        [HttpPut("/categoria/{id:int}")]
+        public Categoria Update(Categoria categoria, int id)
+        {
+            var model = _context.Categorias.Find(id);
+            model.CategoriaNome = categoria.CategoriaNome;
+            _context.Categorias.Update(model);
+            _context.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete("/categoria/{id:int}")]
+        public Categoria Delete(int id)
+        {
+            var model = _context.Categorias.Find(id);
+            if(model != null)
+            {
+                _context.Categorias.Remove(model);
+                _context.SaveChanges();
+            }
+            return model;
+        }
     }
 }
